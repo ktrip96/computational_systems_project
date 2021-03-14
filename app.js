3d9c02c8c238f93d8e6056dbf118abb2e8ada5b0
ntziris <ntziris@gmail.com>
2021-03-14 09:46:10 +0200

    first working (beta) version

--- app.js
+++ app.js
@@ -1,11 +1,37 @@
 //* Requirements */
 
-const { returnAknContent, returnPdfBody } = require('./functions')
+const { genTypesMap, getDateString, returnAknContent, returnPdfBody } = require('./functions')
 const axios = require('axios')
 const fs = require('fs')
+const path = require('path')
 const PDFExtract = require('pdf.js-extract').PDFExtract
+const { response } = require('express')
 const pdfExtract = new PDFExtract()
 
+async function download(docUrl) {
+  const url = encodeURI(docUrl);
+
+  const dir = path.resolve(__dirname, 'temp.pdf');
+
+  const resp = await axios({
+    method: 'GET',
+    url: url,
+    responseType: 'stream',
+    authorization: 'Ym9iOmJvYg=='
+  });
+
+  resp.data.pipe(fs.createWriteStream(dir));
+
+  return new Promise((resolve, reject) => {
+    resp.data.on('end', () => {
+      resolve()
+    })
+
+    resp.data.on('error', err => {
+      reject(err)
+    })
+  })
+}
 /*
     Σειρά:
     - Παίρνουμε το ada σαν input
@@ -17,40 +43,97 @@ const pdfExtract = new PDFExtract()
 
 */
 
-const options = {
-  normalizeWhitespace: true,
-} /* see below */
-pdfExtract.extract('file.pdf', options, (err, data) => {
-  if (err) return console.log(err)
-  let arr = data.pages[0].content
-  console.log(returnPdfBody(arr))
-})
+// Ελέγχει αν έχουμε αποθηκεύσει το αρχείο, έτσι ώστε να αποφεύγουμε τα διπλότυπα.
+const isDuplicate = (ada, publishDate) => {
+  const fileName = ada + '_' + getDateString(publishDate) + '.akn';
+  const dir = path.resolve(__dirname, 'documents');
+  let returnState = false;
+  // list all files in the directory
+  try {
+    const files = fs.readdirSync(dir);
+    // files object contains all files names
+    // log them on console
+    files.forEach(file => {
+      if (file === fileName) {
+        console.log('File already exists.');
+        console.log('File: ' + dir + '/' + fileName);
+        returnState = true;
+      }
+    });
+  } catch (err) {
+    console.log(err);
+  } finally {
+    return returnState;
+  }
+};
 
 //* GET REQUESTS */
 
-// axios({
-//   method: 'get',
-//   url: 'https://test3.diavgeia.gov.gr/luminapi/opendata/types',
-//   header: {
-//     Accept: 'application/json',
-//   },
-// })
-//   .then((resp) => {
-//     console.log(resp.data.decisionTypes[0][target])
-//     let result = resp.data.decisionTypes[0][target]
-//     content = replaceBetween(content, firstIndex, finalIndex, result)
-//     console.log({ newFile: content })
+axios.all([
+  axios.get('https://diavgeia.gov.gr/luminapi/opendata/types'),
+  axios.get(
+    encodeURI('https://diavgeia.gov.gr/luminapi/opendata/decisions/ΩΑ6Ρ465ΧΘΟ-8ΛΡ')
+  ),
+  axios.get(
+    encodeURI('https://diavgeia.gov.gr/luminapi/opendata/decisions/ΩΑ6Ρ465ΧΘΟ-8ΛΡ/versionlog.json')
+  )
+])
+  .then(resps => {
+    const typesMap = genTypesMap(resps[0].data.decisionTypes);
+    const publishDate = new Date(resps[1].data.publishTimestamp);
 
-//     try {
-//       const data = fs.writeFileSync(
-//         '/home/ktrip96/computational_systems_project/final.akn',
-//         content
-//       )
-//       //file written successfully
-//     } catch (err) {
-//       console.error(err)
-//     }
-//   })
-//   .catch((error) => {
-//     console.log(error)
-//   })
+    if (!(isDuplicate(resps[1].data.ada, publishDate))) {
+      download(resps[1].data.documentUrl).then(() => {
+        const options = {
+          normalizeWhitespace: true,
+        };
+        pdfExtract.extract('temp.pdf', options, (err, data) => {
+          if (err) return console.log(err);
+          let arr = data.pages[0].content;
+          axios
+            .all([
+              axios.get(
+                encodeURI(
+                  'https://diavgeia.gov.gr/luminapi/opendata/signers/' +
+                    resps[1].data.signerIds[0] +
+                    '.json'
+                )
+              ),
+              axios.get(
+                encodeURI(
+                  'https://diavgeia.gov.gr/luminapi/opendata/organizations/' +
+                    resps[1].data.organizationId +
+                    '.json'
+                )
+              ),
+            ])
+            .then(response => {
+              const dir = path.resolve(
+                __dirname,
+                'documents',
+                resps[1].data.ada + '_' + getDateString(publishDate) + '.akn'
+              );
+              fs.writeFileSync(
+                dir,
+                returnAknContent(
+                  typesMap,
+                  resps[1].data,
+                  resps[2].data.versions,
+                  returnPdfBody(arr),
+                  response[0].data,
+                  response[1].data
+                )
+              );
+              console.log('File: ' + dir);
+            });
+          fs.unlink('temp.pdf', err => {
+            if (err) {
+              throw err;
+            }
+          });
+        });
+      });
+    }
+  }).catch((error) => {
+    console.log(error)
+  });
--- functions.js
+++ functions.js
@@ -1,13 +1,114 @@
-const returnAknContent = (object) => {
-  return `<FRBRWork label=${object.label}>
-            <FRBRthis value="/gr/pd/2015/1/"/>
-            <FRBRuri value="/gr/pd/2015/1/"/>
-            <FRBRdate date="2015-01-09" name="Publish"/>
-            <FRBRdate date="2015-01-09" label="Kostas"/>
-            <FRBRauthor as="#author" href="#"/>
-            <FRBRcountry value="gr"/>
-          </FRBRWork>`
+const genTypesMap = decisionTypes => {
+  let typesMap = new Map();
+  decisionTypes.forEach(element => {
+    typesMap.set(element.uid, element.label);
+  });
+  return typesMap;
+};
+
+
+const getDateString = (date) => {
+  return date.getFullYear() +
+    '-' +
+    ('0' + (date.getMonth() + 1)).slice(-2) +
+    '-' +
+    ('0' + date.getDate()).slice(-2);
+};
+
+const returnAknContent = (typesMaps, data, history, body, signer, organization) => {
+  const publishDate = new Date(data.publishTimestamp);
+  const now = new Date();
+    
+  let akn = `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<akomaNtoso xmlns="http://docs.oasis-open.org/legaldocml/ns/akn/3.0/CSD13">
+    <act contains="singleVersion" name="${data.subject}">
+        <meta>
+            <identification source="#HB_bot">
+                <FRBRWork>
+                    <FRBRthis value="/gr/act/${publishDate.getFullYear()}/${
+    data.ada
+  }/"/>
+                    <FRBRuri value="/gr/act/${publishDate.getFullYear()}/${
+    data.ada
+  }/"/>
+                    <FRBRdate date="${getDateString(publishDate)}" name="${
+    data.status
+  }"/>
+                    <FRBRauthor as="#author" href="#"/>
+                    <FRBRcountry value="gr"/>
+                </FRBRWork>
+                <FRBRExpression>
+                    <FRBRthis value="/gr/act/${publishDate.getFullYear()}/${
+    data.ada
+  }/${data.ada}_${getDateString(publishDate)}"/>
+                    <FRBRuri value="/gr/act/${publishDate.getFullYear()}/${
+    data.ada
+  }/${data.ada}_${getDateString(publishDate)}"/>
+                    <FRBRdate date="${getDateString(now)}" name="INSERT"/>
+                    <FRBRauthor as="#editor" href="#HB_bot"/>
+                    <FRBRlanguage language="gr"/>
+                </FRBRExpression>
+                <FRBRManifestation>
+                    <FRBRthis value="/gr/act/${publishDate.getFullYear()}/${
+    data.ada
+  }/${data.ada}_${getDateString(publishDate)}.xml"/>
+                    <FRBRuri value="/gr/act/${publishDate.getFullYear()}/${
+    data.ada
+  }/${data.ada}_${getDateString(publishDate)}.akn"/>
+                    <FRBRdate date="${getDateString(
+                      now
+                    )}" name="PDFConversion"/>
+                    <FRBRauthor as="#editor" href="#HB_bot"/>
+                </FRBRManifestation>
+            </identification>
+            <publication date="${getDateString(publishDate)}" number="${
+    data.protocolNumber
+  }" name="et" showAs="${organization.label}"/>
+            <classification source="#δι@υγεια"/>
+            <lifecycle source="#HB_bot">
+                <eventRef type="generation" date="${getDateString(
+                  publishDate
+                )}"/>
+            </lifecycle>
+            <proprietary source="#HB_bot">
+                <DCTerms xmlns:ns2="http://docs.oasis-open.org/legaldocml/ns/akn/3.0/CSD13" xmlns="" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:dcterms="http://purl.org/dc/terms/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+                    <docIssuerRank>${
+                      signer.units[0].positionLabel
+                    }</docIssuerRank>
+                    <dc:description>${data.subject}</dc:description>
+                    <dc:language>gr</dc:language>
+                    <dc:creator>${signer.firstName} ${
+    signer.lastName
+  }</dc:creator>
+                    <dcterms:created>${getDateString(
+                      publishDate
+                    )}</dcterms:created>
+                    <dc:format>text/xml</dc:format>
+                    <dc:type2>ACT</dc:type2>
+                    <dc:type>${typesMaps.get(data.decisionTypeId)}</dc:type>
+                    <docKYA/>
+                    <dc:title>${data.subject}</dc:title>
+                    <docProtocol/>
+                    <docPol/>
+                    <dc:identifier>/gr/act/${publishDate.getFullYear()}/${
+    data.ada
+  }/${data.ada}_${getDateString(publishDate)}}.akn</dc:identifier>
+                    <dc:publisher>${organization.label}</dc:publisher>
+                </DCTerms>
+            </proprietary>
+        </meta>
+        <body eId="/gr/act/${publishDate.getFullYear()}/${
+    data.ada
+  }/main/" wId="/gr/act/${publishDate.getFullYear()}/${data.ada}/main/">
+ `;
+  akn += body;
+  akn += `
+  </body>
+</akomaNtoso>`;
+  return akn;
 }
+
+
 // H returnPdfBody επιστρέφει ένα string με το body του pdf.
 
 const returnPdfBody = (array) => {
@@ -19,4 +120,4 @@ const returnPdfBody = (array) => {
   return string
 }
 
-module.exports = { returnAknContent, findPdfIndex }
+module.exports = { genTypesMap, getDateString, returnAknContent, returnPdfBody };
